7d76f489c83108e2cdbc514b3383610c5657c19a
szycs <szy.csaba@pizzaforte.hu>
2024-07-02 15:45:43 +0200

    Set img max sizes

--- resources/views/public/post/list.blade.php
+++ resources/views/public/post/list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @foreach ($events as $event)
         <article class="my-6">
-            <img class="max-w-xs max-h-80" src="{{ $event->main_photo }}" alt="Címlapfotó" />
+            <img class="max-w-52 max-h-52" src="{{ $event->main_photo }}" alt="Címlapfotó" />
             <h2 class="text-xl font-bold">
                 <a class="underline hover:text-teal-500"
                     href="{{ route('posts.show', ['id' => $event->id]) }}">{{ $event->name }}</a>
